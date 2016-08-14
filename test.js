68043ccc087ef84961e26130cf6a8e91ca3f9c4d
t32k <ijok.ijok@gmail.com>
2016-08-14 14:25:20 +0900

    Add an test

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import test from 'ava';
+import gulp from 'gulp';
 import gutil from 'gulp-util';
 import defineTpl from './';
 
@@ -8,8 +9,8 @@ var fakeFile;
 const getFakeFile =(fileContent) => {
     return new gutil.File({
         cwd: './',
-        base: './test/fixture/',
-        path: './test/fixture/file.txt',
+        base: './test/fixtures/',
+        path: './test/fixtures/file.txt',
         contents: new Buffer(fileContent || '')
     });
 }
@@ -21,12 +22,28 @@ test.beforeEach(t => {
 test.cb ('Define as a golang template', t => {
     let stream = defineTpl();
 
-    stream.on('data', function (newFile) {
-        t.truthy(newFile);
-        t.is(newFile.contents.toString(), '{{define "file.txt"}}\nHello world\n{{end}}');
+    stream.on('data', function (file) {
+        t.truthy(file);
+        t.is(file.contents.toString(), '{{define "file.txt"}}\nHello world\n{{end}}');
     });
 
     stream.on('end', t.end);
     stream.write(fakeFile);
     stream.end();
+});
+
+test.cb ('Define multiple templates', t => {
+    let stream = gulp.src('./test/fixtures/*').pipe(defineTpl());
+    let files = [];
+
+    stream.on('data', function (file) {
+        t.regex(file.contents.toString(), /^\{\{define "(foo|bar)\.(js|css)"\}\}(.|\n)+\{\{end\}\}/);
+        files.push(file);
+    });
+
+    stream.on('end', () => {
+        t.is(files.length, 2);
+        t.end();
+    });
+    stream.on('error', t.end);
 });
