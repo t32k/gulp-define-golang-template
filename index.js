ddb9572fd944cbe798ded9f687b318ce1d1b66e6
t32k <ijok.ijok@gmail.com>
2016-08-14 13:21:40 +0900

    Add test

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
-'use strict';
+const path = require('path');
+const through = require('through2');
+const Concat = require('concat-with-sourcemaps');
 
-var Concat = require('concat-with-sourcemaps');
-var extend = require('object-assign');
-var through = require('through2');
-var gutil = require('gulp-util');
-var stream = require('stream');
-var path = require('path');
-var fs = require('fs');
-
-module.exports = function () {
-
+module.exports = () => {
     function TransformStream(file, enc, cb) {
-        var filename = path.basename(file.path);
-        var concat = new Concat(false, filename, '\n');
+        let filename = path.basename(file.path);
+        let concat = new Concat(false, filename, '\n');
 
         concat.add(null, new Buffer('{{define "' + filename + '"}}'));
         concat.add(file.relative, file.contents);
@@ -22,6 +15,5 @@ module.exports = function () {
         this.push(file);
 	    cb();
     }
-
     return through.obj(TransformStream);
 };
---
+++ test.js
@@ -0,0 +1,30 @@
+import test from 'ava';
+import gutil from 'gulp-util';
+import defineTpl from './';
+
+var fakeFile;
+const getFakeFile =(fileContent) => {
+    return new gutil.File({
+        cwd: './',
+        base: './test/fixture/',
+        path: './test/fixture/file.txt',
+        contents: new Buffer(fileContent || '')
+    });
+}
+
+test.beforeEach(t => {
+    fakeFile = getFakeFile('Hello world');
+});
+
+test.cb ('Define as a golang template', t => {
+    let stream = defineTpl();
+
+    stream.on('data', function (newFile) {
+        t.truthy(newFile);
+        t.is(newFile.contents.toString(), '{{define "file.txt"}}\nHello world\n{{end}}');
+    });
+
+    stream.on('end', t.end);
+    stream.write(fakeFile);
+    stream.end();
+});
